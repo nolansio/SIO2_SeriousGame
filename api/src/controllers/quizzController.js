1cd26a22c6fff3cf5d2bdd74c359b40a4b68e507
Dorian <dorian.giraud2006@gmail.com>
2026-03-06 13:30:11 +0100

    fix: add a new status code in post quizzes

--- api/src/controllers/quizzController.js
+++ api/src/controllers/quizzController.js
@@ -1,14 +1,15 @@
 const quizzService = require("@services/quizzService");
 
 exports.createQuizz = async (req, res) => {
-  if (!req.user.id || !req.body.title || !req.body.description) {
+  if (!req.user.id || !req.body.title || !req.body.description || !req.params.id) {
     return res
       .status(400)
       .json({ error: "Parameters 'title' and 'description' required" });
   }
-  req.body.userId = req.user.id;
+  
+  req.body.userId = req.params.id;
   try {
-    const newQuizz = await quizzService.create(req.body);
+    const newQuizz = await quizzService.create(req.body, req.user);
     res.status(201).json(newQuizz);
   } catch (error) {
     if (error.message === "USER_NOT_FOUND") {
@@ -46,6 +47,18 @@ exports.updateQuizz = async (req, res) => {
   }
 };
 
+exports.getQuizz = async (req, res) => {
+  try {
+    const newQuizz = await quizzService.getQuizz(req.params.id);
+    res.status(200).json(newQuizz);
+  } catch (error) {
+    if (error.message === "QUIZZ_NOT_FOUND") {
+      return res.status(404).json({ error: error.message });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
+};
 // exports.getUserById = async (req, res) => {
 //   const user = await userService.findById(req.params.id);
 //   if (!user) {
--- api/src/services/quizzService.js
+++ api/src/services/quizzService.js
@@ -1,4 +1,4 @@
-const { User, Quizz } = require("@models");
+const { User, Quizz, Question } = require("@models");
 
 async function generateUniqueCode() {
   const datePart = Date.now().toString(36).slice(-4);
@@ -13,8 +13,13 @@ async function generateUniqueCode() {
   return code;
 }
 
-exports.create = async (data) => {
+exports.create = async (data, currentUser) => {
   const user = await User.findByPk(data.userId);
+  if (currentUser.role === "USER" && currentUser.id != data.userId) {
+    const error = new Error("An User can't create a Quizz for another User");
+    error.code = "FORBIDDEN";
+    throw error;
+  }
   if (!user) {
     const error = new Error("User not found");
     error.code = "USER_NOT_FOUND";
@@ -38,5 +43,22 @@ exports.update = async (data) => {
     throw error;
   }
   await quizz.update(data);
+  return quizz;
+};
+
+exports.getQuizz = async (id) => {
+  const quizz = await Quizz.findByPk(id, {
+    include: [
+      {
+        model: Question,
+        as: "questions",
+      },
+    ],
+  });
+  if (!quizz) {
+    const error = new Error("Quizz not found");
+    error.code = "QUIZZ_NOT_FOUND";
+    throw error;
+  }
   return quizz;
 };
